178e1ccbe86c2fbf42032157475724f4e936aa6b
Lukasz <lukaszcwiacz@gmail.com>
2025-01-02 16:37:50 +0100

    Added card_payment method in bank_app_hanlder.py

--- bank_app_handler.py
+++ bank_app_handler.py
@@ -37,6 +37,39 @@ class BankDataBaseHandler:
             cursor.execute(query, (user_email,))
             self.conn.commit()
 
+    def card_payment(self, user_email, amount):
+        try:
+            with self.conn.cursor() as cursor:
+                query = "SELECT amount FROM bank_app.bank_accounts WHERE user_email = %s"
+                cursor.execute(query, (user_email))
+                result= cursor.fetchone()
+
+                if not result:
+                    print(f"User with email {user_email} does not exist")
+                    return False
+                
+                current_balance=result[0]
+                print(f"Current balance for {user_email}: {current_balance}")
+
+                if current_balance<amount:
+                    print(f"Insufficient funds for {user_email}")
+                    return False
+                
+                new_balance = current_balance - amount
+                update_query = "UPDATE bank_app.bank.accounts SET amount = %s WHERE user_email = %s"
+                cursor.execute(update_query, (new_balance,user_email))
+                self.conn.commit()
+                print(f"Payment of {amount} processed for {user_email}. New balance: {new_balance}")
+                return True
+        
+        except psycopg2.Error as e:
+            print(f"Error processing payment: {e}")
+            self.conn.rollback()
+            raise
+
+# funckja płacenia kartą, podawanie kwoty do zapłaty i email. Sprawdzic czy sa wysttarczajace srodki, pobrac kwote i zaktualizowac
+# wyslac zmiany na GIT i zrobić pull request
+
 bdb_handler=BankDataBaseHandler(host="localhost", database="postgres", user="postgres", password="root")
 bdb_handler2=BankDataBaseHandler(host="localhost", database="postgres", user="postgres", password="root")
 # bdb_handler.add_user(user_email="ggg@gmail.com", amount=1500)
